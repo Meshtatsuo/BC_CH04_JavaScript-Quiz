877051d33126e47491086f7540a14a5ab8622b44
MesH <meshtatsuo@gmail.com>
2022-01-16 18:13:57 -0700

    basic CSS stylings

---
+++ assets/js/gameshow.js
@@ -0,0 +1,12 @@
+// question object contains question, all answers, and index of correct answer
+let question = {
+  qText: "",
+  qAns1: "",
+  qAns2: "",
+  qAns3: "",
+  qAns4: "",
+  correctIndex: 1,
+};
+
+// list of questions
+let questionList = [];
